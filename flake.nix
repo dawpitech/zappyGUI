f55cfefb53fac3e41a56a78ecd4efc834231c1ae
Pomalone <o.c.loulou@gmail.com>
2025-06-02 09:40:20 +0200

    feat: add flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        cc = pkgs.gcc14;
+
+        deps = with pkgs; [
+          libcxx
+          gnumake
+          criterion
+        ] ++ [ cc ];
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            valgrind
+            cmake
+            doxygen
+            pkg-config
+          ] ++ deps;
+        };
+        formatter = pkgs.nixpkgs-fmt;
+      });
+}
