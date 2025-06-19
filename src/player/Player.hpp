7caba4974b1c9bbaa844df5aaf0a1372edda5a87
Pomalone <o.c.loulou@gmail.com>
2025-06-19 19:01:58 +0200

    feat: add an enum for player orientation

--- src/player/Player.hpp
+++ src/player/Player.hpp
@@ -1,15 +1,21 @@
-#include "IResource.hpp"
 #include <cstddef>
 #include <string>
 #include <vector>
 
+enum orientation {
+    North,
+    East,
+    South,
+    West,
+};
+
 namespace GUI {
     class Player {
         private:
-            std::vector<IResource> _inventory;
             std::size_t _x;
             std::size_t _y;
             std::string _team;
             std::string _name;
+            orientation _o;
     };
 } // namespace GUI
